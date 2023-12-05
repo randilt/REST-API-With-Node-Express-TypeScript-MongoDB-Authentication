c7cadaf98cbb99baccc3f241823deeea16682e25
RandilT <randiltharusha72@gmail.com>
2023-12-05 10:49:46 +0530

    Complete the api

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,11 +1,9 @@
-import crypto from "crypto";
-const SECRET = "TOXICTOXICTOXICTOXICTOXICTOXIC";
+import crypto from 'crypto';
 
-export const random = () => crypto.randomBytes(128).toString("base64");
+const SECRET = 'ANTONIO-REST-API';
 
-export const authentication = (salt: string, password: string) => {
-  return crypto
-    .createHmac("sha256", [salt, password].join("/"))
-    .update(SECRET)
-    .digest("hex");
-};
+export const authentication = (salt: string, password: string): string => {
+  return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('hex');
+}
+
+export const random = () => crypto.randomBytes(128).toString('base64');
--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,39 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
+import router from "./router";
+import mongoose from "mongoose";
 
 const app = express();
-const PORT = 8080;
-const DB_URL = process.env.MONGO_URL; // MongoDB URL
-
+dotenv.config();
 app.use(
   cors({
     credentials: true,
   })
 );
+
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
+const PORT = 8080;
+
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}/ `);
 });
 
+const MONGO_URL = process.env.MONGO_URL; // DB URI
+
 mongoose.Promise = Promise;
-mongoose.connect(DB_URL); // Connecting with the mongoDB database
+mongoose.connect(MONGO_URL); // Connecting with the mongoDB database
 mongoose.connection.on("connected", () => {
   console.log("MongoDB connected successfully");
 });
 mongoose.connection.on("error", (error) => {
   console.error("MongoDB connection error: " + error);
 });
+
+app.use("/", router());
